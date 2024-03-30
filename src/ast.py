8d934ae2aef87471a0e0025994f76db7d9fb356c
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2024-03-30 09:39:01 +0800

    feat: simplify print string generation by using a list

--- src/ast.py
+++ src/ast.py
@@ -1,3 +1,4 @@
+from typing import Any, List
 from .grammar import BinaryExpr, Expr, ExprElem, LiteralExpr, UnaryExpr, GroupExpr
 from .token import TokenType
 
@@ -11,97 +12,107 @@ class _AstPrinter(Expr):
     def __init__(self, expr: ExprElem) -> None:
         self.expr = expr
         self.indent_level = 0
-        self.print_string = ""
+        self.print_expr_list: List[tuple[str, int, Any]] = []
 
 
     def print(self):
         self.visit_expressions(self.expr)
-        print(self.print_string)
+        print(self.get_print_str())
 
 
-    def add_indent(self, indent_level: int | None=None):
-        indent_level = indent_level if indent_level is not None else self.indent_level
-        if self.indent_level > 1:
-            self.print_string += "┣" + ("━" * (indent_level - 1))
-        elif self.indent_level == 1:
-            self.print_string += "┣"
+    def get_print_str(self):
+        print_str = ""
+        total_list = len(self.print_expr_list) - 1
+        for index, item in enumerate(self.print_expr_list):
+            token, indent_level, value = item
+            if indent_level > 1:
+                start_prefix = "┣" if index < total_list else "┗"
+                print_str += start_prefix + "━" * (indent_level - 1)
+            print_str += f"[{token}]"
+            if value is not None:
+                print_str += f" {value}"
+            if index < total_list:
+                print_str += "\n"
+        return print_str
+
+
+    def add_print_expr_item(self, expr: str, value: Any = None):
+        self.print_expr_list.append((expr, self.indent_level, value))
 
 
     def visit_expressions(self, expr):
-        self.add_indent()
         self.indent_level += 1
         match expr:
             case _bin if isinstance(expr, BinaryExpr):
-                self.print_string += "[BINARY]\n"
                 self.visit_binary(_bin)
                 self.indent_level -= 1
             case _group if isinstance(expr, GroupExpr):
-                self.print_string += "[GROUP]\n"
                 self.visit_group(_group)
                 self.indent_level -= 1
             case _unary if isinstance(expr, UnaryExpr):
-                self.print_string += "[UNARY] "
                 self.visit_unary(_unary)
                 self.indent_level -= 1
             case _literal if isinstance(expr, LiteralExpr):
-                self.print_string += "[LITERAL] "
                 self.visit_literal(_literal)
 
 
     def visit_binary(self, expr):
+        self.add_print_expr_item('BINARY')
         self.visit_expressions(expr.left)
-        self.print_string += "\n"
-        self.add_indent(self.indent_level - 1)
-        self.print_string += "[OPERATOR] "
+        operator = ""
         match expr.operator:
             case TokenType.EQUAL_EQUAL:
-                self.print_string += "=="
+                operator += "=="
             case TokenType.BANG_EQUAL:
-                self.print_string += "!="
+                operator += "!="
             case TokenType.GT:
-                self.print_string += ">"
+                operator += ">"
             case TokenType.LT:
-                self.print_string += "<"
+                operator += "<"
             case TokenType.GT_EQUAL:
-                self.print_string += ">="
+                operator += ">="
             case TokenType.LT_EQUAL:
-                self.print_string += "<="
+                operator += "<="
             case TokenType.PLUS:
-                self.print_string += "+"
+                operator += "+"
             case TokenType.MINUS:
-                self.print_string += "-"
+                operator += "-"
             case TokenType.STAR:
-                self.print_string += "*"
+                operator += "*"
             case TokenType.STAR_STAR:
-                self.print_string += "**"
+                operator += "**"
             case TokenType.SLASH:
-                self.print_string += "/"
+                operator += "/"
             case TokenType.SLASH_SLASH:
-                self.print_string += "//"
-        self.print_string += "\n"
+                operator += "//"
+        self.add_print_expr_item('OPERATOR', operator)
         self.indent_level -= 1
         self.visit_expressions(expr.right)
 
 
     def visit_group(self, expr):
+        self.add_print_expr_item('GROUP')
         self.visit_expressions(expr.group)
 
 
     def visit_unary(self, expr):
+        symbol = ""
         match expr.symbol:
             case TokenType.NOT:
-                self.print_string += "not"
+                symbol += "not"
             case TokenType.MINUS:
-                self.print_string += "-"
-        self.print_string += "\n"
+                symbol += "-"
+        self.add_print_expr_item('UNARY', symbol)
         self.visit_expressions(expr.unary)
 
 
     def visit_literal(self, expr):
+        literal = ""
         if expr.literal is None:
-            self.print_string += "nil"
+            literal += "nil"
         else:
-            self.print_string += str(expr.literal)
+            literal += str(expr.literal)
+        self.add_print_expr_item('LITERAL', literal)
 
 
     def visit_not_implemented_expr(self, expr):
